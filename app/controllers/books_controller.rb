a268f2bf52bdaf001e6eb70c04b4f7f7f21d5093
AsoYusuke <aso.aso.1227@i.softbank.jp>
2021-03-15 14:25:29 +0000

    add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,10 @@ class BooksController < ApplicationController
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
   end
 
   def index
@@ -14,4 +18,11 @@ class BooksController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
