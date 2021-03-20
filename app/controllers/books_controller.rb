4f6c415ddef8d7e09b4faa417d024726a9273401
AsoYusuke <aso.aso.1227@i.softbank.jp>
2021-03-20 17:08:55 +0000

    new

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to book_path(@book.id)
   end
 
   def index
@@ -16,10 +16,15 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.find(params[:id])
+    @book = Book.new
+    @bookdetail = Book.find(params[:id])
+    @user = User.find(current_user.id)
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,26 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
   end
 
   def index
-    @user = User.all
-
+    @users = User.all
+    @book = Book.new
+    @user = User.find(current_user.id)
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path(@book.id)
   end
 
   def edit
     @user = User.find(params[:id])
+    @book = Book.new
   end
 
   def update
@@ -19,6 +30,10 @@ class UsersController < ApplicationController
   end
 
   private
+  
+  def book_params
+    params.require(:book).permit(:profile_image, :title, :body)
+  end
 
   def user_params
     params.require(:user).permit(:name, :profile_image)
